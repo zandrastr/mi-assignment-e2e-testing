4d94e861ca6685019b69ec15b25734cdaa2818e3
Sandra S <zandrastr@hotmail.com>
2023-02-09 22:36:52 +0100

    ✅ Added tests for input and button

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -16,3 +16,24 @@ describe("checks url and start page elements", () => {
     });
 
 });
+
+describe("checks input and button functionality", () => {
+
+    it("should add text to input field", () => {
+        cy.get("input").type("Test input").should("have.value", "Test input");
+    });
+
+    it("should be able to press button", () => {
+        cy.get("input").type("Test input").should("have.value", "Test input");
+        cy.get("button").contains("Sök").click();
+        cy.get("#movie-container").should("exist");
+    });
+
+    it("should show error message", () => {
+        cy.get("input").type(" ").should("have.value", " ");
+        cy.get("button").contains("Sök").click();
+        cy.get("#movie-container").should("exist");
+        cy.get("#movie-container p").contains("Inga sökresultat att visa").should("exist");
+    });
+
+});
